4f5492eebee1ebcd6ffe7e5d8c4419be6bb118c8
Fernando <ramirez.fernando2003@gmail.com>
2022-05-06 00:33:50 -0500

    added rest of sliders and post requests in movement.js

--- website/api/fingers.js
+++ website/api/fingers.js
@@ -1,11 +1,11 @@
 let express = require('express');
 let jsonfile = require("jsonfile");
 let router = express.Router();
-
-let fingers = "These are the fingers";
-let http = require('http');
-
 const jsonPath = './data/fingers.json';
+
+// Johnny five
+var five = require("johnny-five");
+var board = new five.Board();
 
 var getDataFromJson = () => {
    return jsonfile.readFileSync(jsonPath);
@@ -14,29 +14,50 @@ var getDataFromJson = () => {
 //Routes will go here
 router.get('/', function(req, res){
    const jsonData = getDataFromJson();
-
    res.status(200).send(jsonData);
 });
 
-router.post('/', (req, res) => {
-   res.setHeader("Access-Control-Allow-Origin", "*")
-   res.setHeader("Access-Control-Allow-Credentials", "true");
-   res.setHeader("Access-Control-Max-Age", "1800");
-   res.setHeader("Access-Control-Allow-Headers", "content-type");
-   res.setHeader( "Access-Control-Allow-Methods", "POST, GET" ); 
+board.on("ready", function() {
+   var thumb = new five.Servo(1);
+   var index = new five.Servo(2);
+   var middle = new five.Servo(3);
+   var ring = new five.Servo(4);
+   var pinky = new five.Servo(5);
 
 
-   const curVal = req.body.value;
+   router.post('/', (req, res) => {
+      res.setHeader("Access-Control-Allow-Origin", "*")
+      res.setHeader("Access-Control-Allow-Credentials", "true");
+      res.setHeader("Access-Control-Max-Age", "1800");
+      res.setHeader("Access-Control-Allow-Headers", "content-type");
+      res.setHeader( "Access-Control-Allow-Methods", "POST, GET" ); 
 
-   const newJson = {
-      value: curVal
-   };
+     
 
-   jsonfile.writeFile(jsonPath, newJson);
+      const curThumb = req.body.thumb;
+      const curIndex = req.body.index;
+      const curMiddle = req.body.middle;
+      const curRing = req.body.ring;
+      const curPinky = req.body.pinky;
 
-   res.json(newJson);
+      thumb.to(curThumb);
+      index.to(curIndex);
+      middle.to(curMiddle);
+      ring.to(curRing);
+      pinky.to(curPinky);
 
- });
+      const newJson = {
+         thumb: curThumb,
+         index: curIndex,
+         middle: curMiddle,
+         ring: curRing,
+         pinky: curPinky
+      };
 
- 
+      jsonfile.writeFile(jsonPath, newJson);
+      res.json(newJson);
+
+   });
+
+});
 module.exports = router;
