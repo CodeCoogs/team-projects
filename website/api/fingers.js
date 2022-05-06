b446349305662b0c2461e9acae2004d192e7cf70
Fernando <ramirez.fernando2003@gmail.com>
2022-05-05 19:51:50 -0500

    api post and get stuff

--- website/api/fingers.js
+++ website/api/fingers.js
@@ -1,10 +1,42 @@
 let express = require('express');
+let jsonfile = require("jsonfile");
 let router = express.Router();
+
 let fingers = "These are the fingers";
+let http = require('http');
+
+const jsonPath = './data/fingers.json';
+
+var getDataFromJson = () => {
+   return jsonfile.readFileSync(jsonPath);
+}
 
 //Routes will go here
 router.get('/', function(req, res){
-   res.send(fingers);
+   const jsonData = getDataFromJson();
+
+   res.status(200).send(jsonData);
 });
 
+router.post('/', (req, res) => {
+   res.setHeader("Access-Control-Allow-Origin", "*")
+   res.setHeader("Access-Control-Allow-Credentials", "true");
+   res.setHeader("Access-Control-Max-Age", "1800");
+   res.setHeader("Access-Control-Allow-Headers", "content-type");
+   res.setHeader( "Access-Control-Allow-Methods", "POST, GET" ); 
+
+
+   const curVal = req.body.value;
+
+   const newJson = {
+      value: curVal
+   };
+
+   jsonfile.writeFile(jsonPath, newJson);
+
+   res.json(newJson);
+
+ });
+
+ 
 module.exports = router;
